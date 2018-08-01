03591f55bdb182db2e3db148627484b8ce81e11d
PierreGuilmin <pierre.guilmin@mines-paristech.fr>
2018-08-01 19:00:04 -0400

    script improvement

--- data/oncokb/annotate_with_oncokb_annotator.sh
+++ data/oncokb/annotate_with_oncokb_annotator.sh
@@ -6,15 +6,16 @@ Rscript prepare_for_annotation.R
 
 # create a 2.7 python virtual env
 source `which virtualenvwrapper.sh` # find the path to use virtualenvwrapper functions
-printf "\n -> Create a python 2.7 virtual env...\n"
+printf "\n-> Create a python 2.7 virtual env...\n"
 mkvirtualenv --python=python2.7 oncokb-annotator_env > /dev/null
+printf "\n-> Install matplotlib and its dependencies...\n"
 pip install matplotlib > /dev/null
 
 # launch oncokb-annotator
-printf "\n -> Launch oncokb-annotator...\n"
+printf "\n-> Launch oncokb-annotator...\n"
 python oncokb-annotator/MafAnnotator.py -i 'ready_to_annotate_cleaned_IMPACT_mutations_180508.txt' -o 'oncokb_annotated_cleaned_IMPACT_mutations_180508.txt' > /dev/null
 
-printf "\n -> Do some cleaning...\n"
+printf "\n-> Do some cleaning...\n"
 # remove the temporary .txt output from prepare_for_annotation.R
 rm 'ready_to_annotate_cleaned_IMPACT_mutations_180508.txt'
 # remove the oncokb-annotator virtualenv
