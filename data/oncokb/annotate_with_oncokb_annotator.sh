92923419aefcd624654de2391f0336840a1aeb8e
PierreGuilmin <pierre.guilmin@mines-paristech.fr>
2018-08-01 17:08:01 -0400

    add install matplotlib

--- data/oncokb/annotate_with_oncokb_annotator.sh
+++ data/oncokb/annotate_with_oncokb_annotator.sh
@@ -10,6 +10,7 @@ Rscript prepare_for_annotation.R
 source `which virtualenvwrapper.sh` # allow the use of virtualenvwrapper functions
 printf "Create a python 2.7 virtual env\n"
 mkvirtualenv --python=python2.7 oncokb-annotator
+pip install matplotlib
 
 # launch a job oncokb-annotator
 #bsub -I -We 59 -R select[internet] 'python oncokb-annotator/MafAnnotator.py -i "ready_to_annotate_cleaned_IMPACT_mutations_180508.txt" -o "oncokb_annotated_cleaned_IMPACT_mutations_180508.txt'
