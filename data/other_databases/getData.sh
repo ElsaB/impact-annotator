95674e1083e0c2d367e8001816117955068b14c8
PierreGuilmin <pierre.guilmin@mines-paristech.fr>
2018-08-02 14:59:39 -0400

    add CGI download in script

--- data/other_databases/getData.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-# get CIViC data
-printf "\n-> Get the raw CIViC data\n"
-# Data downloaded from https://civicdb.org/releases under "Variant Summaries", 01/07/18 version
-curl https://civicdb.org/downloads/01-Jul-2018/01-Jul-2018-VariantSummaries.tsv --output "./CIViC_01-Jul-2018-VariantSummaries.tsv"
