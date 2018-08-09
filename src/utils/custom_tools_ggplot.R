000958558a43cea941456cf3ed8b7026aec8eeec
PierreGuilmin <pierre.guilmin@mines-paristech.fr>
2018-08-09 14:31:46 -0400

    update plots

--- src/utils/custom_tools_ggplot.R
+++ src/utils/custom_tools_ggplot.R
@@ -77,7 +77,7 @@ plot_density <- function(data, feature_name, fill_feature_name = NULL, adjust = 
     notebook_plot_size(width, height)
                              
     plot <- ggplot(data) + geom_density(aes_string(feature_name, fill = fill_feature_name), adjust = adjust, alpha = 0.2) +
-                           scale_fill_manual(values = c("blue", "green"))
+                           scale_fill_manual(values = c("blue", "green", "red"))
     
     for (value in lines)
         plot <- plot + geom_vline(aes_(xintercept = value), linetype = "dashed", color = "red")
