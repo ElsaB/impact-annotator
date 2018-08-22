b615b2fad67c24a0e4193776698fdf0fda8c9e88
Pierre Guilmin <pierre.guilmin@mines-paristech.fr>
2018-08-22 14:20:18 -0400

    minor modifications

--- data/get_data.sh
+++ data/get_data.sh
@@ -4,7 +4,6 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 
-# get the raw IMPACT mutation data
 printf "\n${GREEN}-> Get the raw IMPACT mutation data...${NC}\n"
 # if in cluster we just copy the file, otherwise we scp
 if (echo "$HOSTNAME" | grep -q "selene") || (echo "$HOSTNAME" | grep -q "luna")
@@ -16,14 +15,12 @@ else
 fi
 
 
-# get the cleaned IMPACT mutation data
 printf "\n${GREEN}-> Get the cleaned IMPACT mutation data...${NC}\n"
 printf "It might take some time (around 2 minutes)...\n"
 Rscript utils/get_cleaned_impact.R
 
 
 
-# get the raw key data
 printf "\n${GREEN}-> Get the raw key data...${NC}\n"
 # if in cluster we just copy the file, otherwise we scp
 if (echo "$HOSTNAME" | grep -q "selene") || (echo "$HOSTNAME" | grep -q "luna")
