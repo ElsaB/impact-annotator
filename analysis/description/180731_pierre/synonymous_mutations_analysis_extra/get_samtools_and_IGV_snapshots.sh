2b066b339ffd4aea8fda2117a6704c93ae3247cd
Pierre Guilmin <pierre.guilmin@mines-paristech.fr>
2018-08-30 18:04:34 -0400

    fix bug

--- analysis/description/180731_pierre/synonymous_mutations_analysis_extra/get_samtools_and_IGV_snapshots.sh
+++ analysis/description/180731_pierre/synonymous_mutations_analysis_extra/get_samtools_and_IGV_snapshots.sh
@@ -2,6 +2,7 @@
 
 BLUE='\033[0;34m'
 GREEN='\033[0;32m'
+RED='\033[0;31m'
 NC='\033[0m' # no Color
 
 
@@ -26,8 +27,18 @@ do
 		chr="${array[1]}"
 		pos="${array[2]}"
 
-		OUTPUT_SAM=samtools/${INPUT_BAM##*/} # remove the path name
-		OUTPUT_SAM=${OUTPUT_SAM%.*}.sam 	 # change the file descriptor to .sam
+		INPUT_BAM_FILE_NAME=${INPUT_BAM##*/} # remove the path name
+
+		# if the file doesn't exist in /ifs/dmpshare/share/irb12_245/, we skip the samtools and don't copy it in corrected_list_BAM.tsv, the input of toil_snapigv
+		if (grep -q $INPUT_BAM_FILE_NAME ../list_irb12_245.txt)
+		then
+			echo "$line" >> corrected_list_BAM.tsv
+		else
+			printf "${RED}${INPUT_BAM_FILE_NAME} removed from corrected_list_BAM.tsv${NC}\n"
+			continue
+		fi
+
+		OUTPUT_SAM=samtools/${INPUT_BAM_FILE_NAME%.*}.sam 	 # change the file descriptor to .sam
 		pos1=$((pos-50))
 		pos2=$((pos+50))
 
@@ -50,7 +61,7 @@ do
 		--logFile snaps/head_job.toil \
 		--setEnv TOIL_LSF_JOBNAME='IGVBATCH' \
 		--outdir snaps \
-		--list list_BAM.tsv \
+		--list corrected_list_BAM.tsv \
 		--window 50 \
 		--genome /ifs/work/leukgen/ref/homo_sapiens/GRCh37d5/igv/gr37offline.genome \
 		--igv_jar /ifs/work/leukgen/opt/igv/batch_runner_patch/igv.jar \
