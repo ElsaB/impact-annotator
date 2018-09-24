af4c78e5bef9d2fcc4d6a9b6bf6c8d2c9b1e38cc
PierreGuilmin <pierre.guilmin@mines-paristech.fr>
2018-09-24 18:33:02 -0400

    fix bug

--- data/annotate_final_dataset.sh
+++ data/annotate_final_dataset.sh
@@ -3,5 +3,5 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-printf "\n${GREEN}-> Annotate the final dataset...${NC}\n"
-Rscript -e 'data_path <- "./";source("utils/compute_final_dataset.R");annotate_final_dataset()'
+printf "\n${GREEN}-> Annotate the final dataset (~ 2 minutes)...${NC}\n"
+Rscript -e 'data_path <- ".";source("utils/compute_final_dataset.R");annotate_final_dataset()'
--- data/get_final_dataset.sh
+++ data/get_final_dataset.sh
@@ -4,4 +4,4 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 printf "\n${GREEN}-> Get the final IMPACT mutation data (~ 10 minutes)...${NC}\n"
-Rscript -e 'data_path <- "./";source("utils/compute_final_dataset.R");get_final_dataset()'
+Rscript -e 'data_path <- ".";source("utils/compute_final_dataset.R");get_final_dataset()'
