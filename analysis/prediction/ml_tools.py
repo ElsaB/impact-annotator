badcfff257e2e373b5d7c8a0f6b1905806cf4190
PierreGuilmin <pierre.guilmin@mines-paristech.fr>
2018-09-27 17:01:12 -0400

    work on cluster-notebook link

--- analysis/prediction/ml_tools.py
+++ analysis/prediction/ml_tools.py
@@ -1,3 +1,5 @@
+-*- coding: utf-8 -*-
+
 import pandas as pd
 import numpy as np
 from sklearn.metrics import roc_curve, auc, roc_auc_score
@@ -8,7 +10,7 @@ import time
 def plot_roc_(n_folds, metrics, ax):
     mean_fpr = np.linspace(0, 1, 100) # [0, 0.01, 0.02, ..., 0.09]
     tprs = [] # True Positive Rate for each fold
-    
+
     # plot fold ROC
     for i in range(n_folds):
         fpr, tpr = metrics.iloc[i].test_fpr, metrics.iloc[i].test_tpr
@@ -128,3 +130,88 @@ def run_model(model, X, y, cv_strategy, grid_search = False, print_fold_metrics 
     metrics.drop(['test_fpr', 'test_tpr'], axis = 1, inplace = True)
     
     return (metrics)
+
+
+def load_dataset():
+    impact = pd.read_csv('../../data/annotated_final_IMPACT_mutations_180508.txt', sep = '\t', low_memory = False)
+
+    # shuffle data
+    rng = np.random.RandomState(0)
+    permutation = rng.permutation(len(impact))
+    impact = impact.iloc[permutation]
+
+    impact['is_somatic'] = impact.confidence_class != "UNLIKELY"
+
+    label_feature_name = 'is_somatic'
+
+    impact_selected = pd.concat([impact[~impact.is_somatic],
+                             impact[impact.is_somatic].iloc[0:20000]], ignore_index = True)
+
+    feature_names = [
+    # 'Hugo_Symbol', 'Chromosome', 'Start_Position', 'End_Position', 'Consequence', 'Variant_Type', 'Reference_Allele', 'Tumor_Seq_Allele2', 'Tumor_Sample_Barcode',
+    # 'cDNA_change', 'HGVSp_Short',
+    't_depth', 't_vaf', 't_alt_count', 'n_depth', 'n_vaf', 'n_alt_count',
+    # 't_ref_plus_count', 't_ref_neg_count', 't_alt_plus_count', 't_alt_neg_count',
+    #'confidence_class',
+    'sample_coverage',
+    #'mut_key',
+    #'VAG_VT', 'VAG_GENE', 'VAG_cDNA_CHANGE', 'VAG_PROTEIN_CHANGE', 'VAG_EFFECT',
+    'VEP_Consequence',
+    #'VEP_SYMBOL', 'VEP_HGVSc', 'VEP_HGVSp',
+    #'VEP_Amino_acids',
+    'VEP_VARIANT_CLASS',
+    #'VEP_EXON', 'VEP_INTRON',
+    'VEP_IMPACT',
+    'VEP_CLIN_SIG',
+    'VEP_COSMIC_CNT',
+    'VEP_gnomAD_AF',
+    #'sample_mut_key', 'patient_key',
+    'frequency_in_normals',
+    #'VEP_SIFT_class',
+    #'VEP_SIFT_score',
+    #'VEP_PolyPhen_class',
+    #'VEP_PolyPhen_score',
+    'VEP_in_dbSNP',
+    'VEP_gnomAD_total_AF_AFR',
+    'VEP_gnomAD_total_AF_AMR',
+    'VEP_gnomAD_total_AF_ASJ',
+    'VEP_gnomAD_total_AF_EAS',
+    'VEP_gnomAD_total_AF_FIN',
+    'VEP_gnomAD_total_AF_NFE',
+    'VEP_gnomAD_total_AF_OTH',
+    'VEP_gnomAD_total_AF_max',
+    'VEP_gnomAD_total_AF',
+    'Kaviar_AF',
+    #'is_a_hotspot',
+    #'is_a_3d_hotspot',
+    #'oncogenic',
+    'gene_type',
+    label_feature_name
+    ]
+
+    categorical_features_names = [
+    'VEP_Consequence',
+    #'VEP_Amino_acids',
+    'VEP_VARIANT_CLASS',
+    'VEP_IMPACT',
+    'VEP_CLIN_SIG',
+    #'VEP_SIFT_class',
+    #'VEP_PolyPhen_class',
+    'VEP_in_dbSNP',
+    'gene_type',
+    ]
+
+    impact_selected = impact_selected[feature_names].dropna()
+    impact_selected = pd.get_dummies(impact_selected, columns = categorical_features_names, sparse = True)
+    X = impact_selected.drop(label_feature_name, axis = 1) # features matrix X: [n_samples, n_features]
+    y = impact_selected[label_feature_name]                # target array y: n_samples
+
+    from sklearn.model_selection import StratifiedShuffleSplit
+
+    # returns stratified randomized folds. The folds are made by preserving the percentage of samples for each class.
+    cv_strategy = StratifiedShuffleSplit(n_splits = 5, test_size = 0.2, random_state = 1)
+
+    return (X, y, cv_strategy)
+
+
+
