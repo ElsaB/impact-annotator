1dc270d2f2759fbe8101f0a6217aa0a0a40d9e98
Pierre Guilmin <pierre.guilmin@mines-paristech.fr>
2018-10-24 17:48:06 -0400

    various work

--- analysis/prediction/summary.py
+++ analysis/prediction/summary.py
@@ -64,7 +64,7 @@ class Summary():
 
 
 
-    def plot(self, figsize=(20, 10)):
+    def plot(self, figsize=(20, 10), fontsize=10):
         summary_transpose = self.summary.copy().transpose()
 
         fig, ax = plt.subplots(1, 1, figsize=figsize)
@@ -74,18 +74,19 @@ class Summary():
         std_metrics.index = mean_metrics.index
 
         mean_metrics.plot.bar(ax=ax, width=0.85, color=self.summary['color'],
-                               yerr=std_metrics, error_kw={'ecolor': 'black', 'capsize': 1.5}, linewidth=0)
+                               yerr=std_metrics, error_kw={'ecolor': 'black', 'capsize': 2}, linewidth=0)
             
         # print text results
         for rect in ax.patches:
             ax.text(rect.get_x() + rect.get_width() / 2, rect.get_height() + 0.01 + std_metrics.max().max(),
-                    '{:.3f}'.format(rect.get_height()), ha='center', va='bottom', color=rect.get_facecolor(), fontsize=10, rotation=55)
+                    '{:.3f}'.format(rect.get_height()), ha='center', va='bottom', color=rect.get_facecolor(), fontsize=fontsize, rotation=55)
 
         xmin, xmax = ax.get_xlim()
         plt.plot(ax.get_xlim(), [1.0, 1.0], '--', alpha=0.5, linewidth=1, color='navy')
         plt.xticks(rotation=0)
         plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), prop={'size': 18})
         plt.ylim(top=1.1)
+
 
 
     def save(self, summary_name):
@@ -114,24 +115,38 @@ class Summary():
         plt.legend()
 
 
-    def plot_2_vs_2(self, figsize=(4, 7), scoring=None):
+    def plot_2_vs_2(self, figsize=(40, 7), scoring=None):
         if not scoring:
             scoring = self.scoring
 
-        plt.figure(figsize=(figsize[0] * len(scoring), figsize[1]))
+        plt.figure(figsize=figsize)
 
         for i, score_name in enumerate(scoring):
             plt.subplot(1, len(scoring), i + 1)
             plt.title(score_name)
 
-            metric_x = self.metrics_dict.values[0].get_metrics()['test_{}'.format(score_name)]
-            metric_y = self.metrics_dict.values[0].get_metrics()['test_{}'.format(score_name)]
+            metric_x = list(self.metrics_dict.values())[0].get_metrics()['test_{}'.format(score_name)]
+            metric_y = list(self.metrics_dict.values())[1].get_metrics()['test_{}'.format(score_name)]
 
-            plt.plot(metric_x, metric_y, style='o', alpha=0.7, label='rel')
-            plt.plot(plt.gca().get_xlim(), plt.gca().get_ylim())
-            print(ttest_rel(metric_x, metric_y))
+            plt.plot(metric_x, metric_y, 'o', alpha=0.6, label='rel')
+            plt.xlabel(list(self.metrics_dict.keys())[0])
+            plt.ylabel(list(self.metrics_dict.keys())[1])
 
-        plt.legend()
+
+            xmin, xmax = plt.gca().get_xlim()
+            ymin, ymax = plt.gca().get_ylim()
+
+            new_min = min(xmin, ymin)
+            new_max = max(xmax, ymax)
+            
+            plt.plot([new_min, new_max], [new_min, new_max])
+            ttest = ttest_rel(metric_x, metric_y)
+            p_value = ttest[1]
+            title = plt.title(score_name + ' (p={:.2e})'.format(p_value))
+
+            if p_value > 0.01:
+                plt.setp(title, color='r')
+
 
 
 
